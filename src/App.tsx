3e080635a080592ad7b2caa3b6044637e09c902e
leo <leorejwan@gmail.com>
2022-04-04 17:41:07 -0300

    State on the Component Button. Increment + 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,7 @@ import { Button } from './components/Button'
 function App() {
   return (
     <>
-      <Button text="button 1" />
-      <Button text="button 2" />
-      <Button text="button 3" />
-      <Button>this is a child</Button>
+      <Button/>
       <Button/>
     </>
   );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,17 @@
-type ButtonProps = {
-    text?: string,
-    children?: string;
-}
+import { useState } from "react"
 
-export function Button(props: ButtonProps){
+export function Button(){
+    const [counter, setCounter] = useState(0)
+
+    function increment(){
+        setCounter(counter + 1)
+        console.log(counter + 1)
+    }
+    
     return (
-        <button>{props.text || props.children || 'default'}</button>
-    );
+        <button onClick={increment}>
+            {counter}
+        </button>
+    )
 }
 

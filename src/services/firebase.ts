67b7a96243bce896186e56fafeb309cb5e9a3555
leo <leorejwan@gmail.com>
2022-04-05 11:18:08 -0300

    environment variables with .env.local

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD-DaPM_nABxUcT9AERHRPQqorQ465DIIs",
-    authDomain: "letmeask-8a4f4.firebaseapp.com",
-    databaseURL: "https://letmeask-8a4f4-default-rtdb.firebaseio.com",
-    projectId: "letmeask-8a4f4",
-    storageBucket: "letmeask-8a4f4.appspot.com",
-    messagingSenderId: "810345324758",
-    appId: "1:810345324758:web:250f7fdc8844b4c6092977"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
 firebase.initializeApp(firebaseConfig);

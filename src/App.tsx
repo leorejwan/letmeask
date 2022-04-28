7c5fd7c36d5e303b97704ff31625f44db878dbe2
leo <leorejwan@gmail.com>
2022-04-28 15:56:20 -0300

    New page Home.tsx. Use Home at App.tsx as a component. Imported the Images of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Button } from './components/Button'
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <>
-      <Button/>
-      <Button/>
-    </>
+    <Home />
   );
 }
 

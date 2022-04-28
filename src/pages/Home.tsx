518e1f9e0db460cb7dffffa0e489579fca6c044e
leo <leorejwan@gmail.com>
2022-04-28 16:32:49 -0300

    created the style of the separator. Line &::before and &::after the content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,7 +19,7 @@ export function Home() {
                         <img src={googleIconImg} alt="logo do Google" />
                         Crie sua sala com o Google
                     </button>
-                    <div>ou entre em uma sala</div>
+                    <div className='separator'>ou entre em uma sala</div>
                     <form>
                         <input type="text" placeholder="Digite ocódigo da sala"></input>
                         <button type="submit">Entrar na sala</button>

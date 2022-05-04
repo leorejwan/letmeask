5fbb663bdc0e28095e77e2c698bb946796391814
leo <leorejwan@gmail.com>
2022-05-04 12:50:08 -0300

    New props: likeCount and hasLiked. off firebase method to detach useEffect on firebase

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
 
 type QuestionType = {
     id: string;
@@ -20,9 +21,13 @@ type FirebaseQuestions = Record<string, {
     content: string;
     isAnswered: boolean;
     isHighlighted: boolean;
+    likes: Record<string, {
+        authorId: string
+    }>;
 }>
 
 export function useRoom(roomId : string) {
+    const { user } = useAuth();
     const [questions, setQuestions] = useState<QuestionType[]>([]);
     const [title, setTitle] = useState('');
 
@@ -41,7 +46,9 @@ export function useRoom(roomId : string) {
                     content: value.content,
                     author: value.author,
                     isHighlighted: value.isHighlighted,
-                    isAnswered: value.isAnswered
+                    isAnswered: value.isAnswered,
+                    likeCount: Object.values(value.likes ?? {}).length,
+                    hasLiked: Object.values(value.likes ?? {}).some(Like => Like.authorId === user?.id)
                 }
             })
             console.log(parsedQuestions);
@@ -49,7 +56,12 @@ export function useRoom(roomId : string) {
             setTitle(databaseRoom.title);
             setQuestions(parsedQuestions);
         })
-    }, [roomId])
+
+        return() =>{
+            roomRef.off('value');
+        }
+
+    }, [roomId, user?.id])
 
     return { questions, title }
 }

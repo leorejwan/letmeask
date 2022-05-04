0668c4fbff1a5fde5136c8357fcbab36d7faa8eb
leo <leorejwan@gmail.com>
2022-05-04 10:31:33 -0300

    Refactoring: Rename Question to QuestionType; attribute key on map

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -21,7 +21,8 @@ type FirebaseQuestions = Record<string, {
     isHighlighted: boolean;
 }>
 
-type Question = {
+type QuestionType = {
+    id: string;
     author: {
         name: string;
         avatar: string;
@@ -41,7 +42,7 @@ export function Room(){
     const roomId = params.id;
 
     const [newQuestion, setNewQuestion] = useState('');
-    const [questions, setQuestions] = useState<Question[]>([]);
+    const [questions, setQuestions] = useState<QuestionType[]>([]);
     const [title, setTitle] = useState('');
 
     useEffect(() => {
@@ -135,6 +136,7 @@ export function Room(){
                     {questions.map((question) => {
                         return (
                             <Question 
+                                key={question.id}
                                 content={question.content}
                                 author={question.author} 
                             />
